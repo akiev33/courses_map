d3831ef19155f4c9cde15c0c221f1c0a047a6e1a
akiev33 <eldarakiev33@gmail.com>
2022-04-10 18:58:43 +0600

    email fix 1

--- authentications/email.py
+++ authentications/email.py
@@ -7,7 +7,7 @@ def email_send(user):
     context = {
         'text_detail': 'Спасибо за регистрацию',
         'email': user.email,
-        'domain': 'http://courses-map.herokuapp.com/',
+        'domain': 'http://courses-map.herokuapp.com',
         'activation_code': user.activation_code,
     }
 
